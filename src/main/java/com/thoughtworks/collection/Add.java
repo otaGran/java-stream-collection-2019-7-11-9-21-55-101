7cf7221c6e4013efeea9a53c94f8e30ae65680e5
beans <ujt2h8@gmail.com>
2019-07-12 06:00:44 +0800

    feat : implement getTripleOfOddAndAddTwo

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -3,6 +3,7 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Add {
@@ -25,7 +26,14 @@ public class Add {
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream()
+                .map((number) ->{
+                    if(number % 2 == 0){
+                        return number;
+                    }else
+                        return number * 3 + 2;
+                } )
+                .collect(Collectors.toList());
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
