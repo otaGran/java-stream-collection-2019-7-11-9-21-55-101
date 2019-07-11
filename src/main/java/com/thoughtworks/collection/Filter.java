3467374e202d36a8721838043bbe4819f4142da4
beans <ujt2h8@gmail.com>
2019-07-12 06:34:17 +0800

    feat : implement filterMultipleOfThree

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -25,7 +25,12 @@ public class Filter {
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+
+
+        return array.
+                stream()
+                .filter(v -> v%3==0)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
