d82fb312f8eb8de6626954e4ed4d662ff4f92002
beans <ujt2h8@gmail.com>
2019-07-12 06:21:52 +0800

    feat : implement popEvenElments

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,10 +2,12 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
@@ -24,11 +26,29 @@ public class CollectionOperator {
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        if(left > right)
+
+            return IntStream.rangeClosed(Math.min(left, right), Math.max(left, right))
+                    .filter(v -> v%2==0)
+                    .boxed()
+                    .sorted(Comparator.reverseOrder())
+                    .collect(Collectors.toList());
+        else
+            return IntStream.rangeClosed(Math.min(left, right), Math.max(left, right))
+                    .filter(v -> v%2==0)
+                    .boxed()
+                    .sorted()
+                    .collect(Collectors.toList());
+
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        Stream<Integer> stream1 = Arrays.stream(array).boxed();
+
+        return stream1
+                .filter(v -> v%2==0)
+                .collect(Collectors.toList());
+
     }
 
     public int popLastElment(int[] array) {
