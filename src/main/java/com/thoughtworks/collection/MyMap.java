f6dd95c2a2a223687e7430da79be489fb096ce47
beans <ujt2h8@gmail.com>
2019-07-12 06:43:45 +0800

    feat : implement mapLetter

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -25,7 +25,11 @@ public class MyMap {
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        return array.stream()
+                .map(v -> letters[v-1])
+
+                .collect(Collectors.toList());
+
     }
 
     public List<String> mapLetters() {
